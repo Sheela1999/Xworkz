bd59afd7bf3aa921fd2aee8bf9d6bd882e118229
Sheela1999 <sheela.xworkz@gmail.com>
2023-03-23 22:54:04 +0530

    second-push

---
+++ College.java
@@ -0,0 +1,57 @@
+public class College{
+	final String name;
+	      int noOfStudents;
+	      float resultOfCollege;
+	      byte noOfTeachers;
+	      char sections;
+	      short fees;
+	      boolean isThisCollegeGoodOrNot;
+	      double studentsResult;
+	static long budget;
+	final String founderName;
+	final String Architecture;
+	      short noOfClassRooms;
+	      long noOfBenches;
+	static byte noOfLabs;
+	static char labSections;
+	      boolean isthisCollegehavingGoodCourses;
+	final String collegeCode;
+	      float qualityOfTeachingInPercent;
+	static short noOfSubjects;
+	static int teachersSalary;
+    final String collegeAdress;
+
+public void giveEducation(){
+	
+}
+public void giveGoodStudents(){
+	
+}
+public void givePlatformForStudents(){
+	
+}
+public void giveComfortableEducation(){
+	
+}	
+public void givesTheNNumberOfPlatformsToLearn(){
+	
+}
+public void givesTheImpressionToSociety(){
+	
+}
+public void collegeGivesPlatformToReacheTheStudentDreams(){
+	
+}
+public void itFindTheStudentsDifferentSkills(){
+	
+}
+public void giveTheKnowledge(){
+	
+}
+public void givesTheEtiquetteToStudents(){
+	
+}
+public void giveGoodMemories(){
+	
+}	
+}
