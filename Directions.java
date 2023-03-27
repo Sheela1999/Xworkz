7493ccabb8b99122df751fc6fe575cbc53f5c75c
Sheela1999 <sheela.xworkz@gmail.com>
2023-03-27 14:10:04 +0530

    second-push

--- Directions.java
+++ Directions.java
@@ -2,28 +2,36 @@ public class Directions{
 	NameOfDirections directionsName;
 	int noOfDirections;
 	static char firstLetterOfDirection;
+	NameOfFourDirections mainFourDirections;
     
     public static void main(String[] args){
 		Directions obj = new Directions();
-        Directions obj1 = new Directions(NameOfDirections.NORTH, 8, 'S');	
+        Directions obj1 = new Directions(NameOfDirections.NORTH, 8, 'S',NameOfFourDirections.EAST);	
 
         System.out.println(obj1.directionsName);
         System.out.println(obj1.noOfDirections);
-        System.out.println(Directions.firstLetterOfDirection);		
+        System.out.println(Directions.firstLetterOfDirection);	
+        System.out.println(obj1.mainFourDirections);		
 	} 
 	
 	public Directions(){
 		System.out.println("Default Constructor");
 	}
 	
-	public Directions(NameOfDirections directionsName,int noOfDirections,char firstLetterOfDirection){
+	public Directions(NameOfDirections directionsName,int noOfDirections,char firstLetterOfDirection,NameOfFourDirections mainFourDirections){
 		this.directionsName = directionsName;
 		this.noOfDirections = noOfDirections;
 		this.firstLetterOfDirection = firstLetterOfDirection;
+		this.mainFourDirections = mainFourDirections;
+		
 	}
 }
 
 enum NameOfDirections{
 	EAST,WEST,SOUTH,NORTH,NORTHWEST,NORTHEAST,SOUTHWEST,SOUTHEAST;
 	
+}
+
+enum NameOfFourDirections{
+	EAST,WEST,SOUTH,NORTH;
 }
